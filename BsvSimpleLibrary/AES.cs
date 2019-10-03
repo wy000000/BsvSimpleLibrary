2f0080febe2366c1e5585f594cfa1a5831c22f3b
yan wu <y.wu@live.com>
2019-10-03 22:37:38 +0800

    aes is added.

---
+++ BsvSimpleLibrary/AES.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using NBitcoin.DataEncoders;
+
+namespace BsvSimpleLibrary
+{
+    public class AES_class
+    {
+        static Aes GetAes(string privateKeyStr)
+        {
+            ASCIIEncoder asciiEncoder = new ASCIIEncoder();
+            byte[] privateKey = asciiEncoder.DecodeData(privateKeyStr);
+            // Create a new instance of the Aes class.
+            Aes myAes = Aes.Create();
+            try
+            {
+                //This generates a new key and initialization vector (IV).                
+                SHA256 sha256 = SHA256.Create();
+                byte[] aesKey = sha256.ComputeHash(privateKey);
+                myAes.Key = aesKey;
+                byte[] aesIV = new byte[16];
+                Array.Copy(sha256.ComputeHash(aesKey), aesIV, 16);
+                myAes.IV = aesIV;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+            return (myAes);
+        }
+        public static byte[] aesEncryptStringToBytes(string plainText, string privateKeyStr)
+        {
+            byte[] encrypted;
+            using (Aes aesAlg = AES_class.GetAes(privateKeyStr))// Aes.Create())
+            {
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+        public static string aesDecryptStringFromBytes(byte[] cipherText, string privateKeyStr)
+        {
+            string plaintext = null;
+            using (Aes aesAlg = AES_class.GetAes(privateKeyStr))
+            {
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+    }
+}
--- BsvSimpleLibraryTest/Program.cs
+++ BsvSimpleLibraryTest/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 //using Newtonsoft.Json;
-//using NBitcoin;
+using NBitcoin;
 using BsvSimpleLibrary;
 
 namespace bsv
@@ -16,7 +16,7 @@ namespace bsv
         static void Main(string[] args)
         {
             string destAddress = "n3Ro8jRU3MNmdhL9KNRkCT6ikQxT26iPaR";//test
-            string privateKey = ""; //test
+            string privateKey = "cSfVAmTktbeKaZ4ZmcYHjoLwdLCmxvE3YP8APf4pnMYKz5YsyM1N"; //test
             string txid = "d6ad0a5fe9f4b7187641b8d9fa9d49754395d338d3fc07887c606a26415a961f";
             string uri = bsvConfiguration_class.bitindexUri;
             string network = bsvConfiguration_class.testNetwork;
@@ -24,24 +24,28 @@ namespace bsv
 
             Dictionary<string, string> response;
 
-            //send bsv and / or write data.
-            response = bsvTransaction_class.send(privateKey, 1000, network, destAddress, null, opReturnData);
+            ////send bsv and / or write data.
+            //response = bsvTransaction_class.send(privateKey, 1000, network, destAddress, null, opReturnData);
 
-            //get opreturn data
-            //byte[] bytes = BitIndex_class.getOpReturnData(uri, network, txid);
-            string s = BitIndex_class.getOpReturnData(uri, network, txid, bsvConfiguration_class.encoding);
+            ////get opreturn data
+            ////byte[] bytes = BitIndex_class.getOpReturnData(uri, network, txid);
+            //string s = BitIndex_class.getOpReturnData(uri, network, txid, bsvConfiguration_class.encoding);
 
-            //get raw tx
-            response = BitIndex_class.getRawTransaction(uri, network, txid);
+            ////get raw tx
+            //response = BitIndex_class.getRawTransaction(uri, network, txid);
 
-            //get tx
-            BitIndexTransaction tx = BitIndex_class.getTransaction(uri, network, txid);
+            ////get tx
+            //BitIndexTransaction tx = BitIndex_class.getTransaction(uri, network, txid);
 
-            //get address Info
-            BitIndexAddressInfo addrInfo = BitIndex_class.getAddressInfo(uri, network, destAddress);
+            ////get address Info
+            //BitIndexAddressInfo addrInfo = BitIndex_class.getAddressInfo(uri, network, destAddress);
 
-            //get utxo
-            BitIndexUtxo_class[] utxos = BitIndex_class.getUtxosByAnAddress(uri, network, destAddress);
+            ////get utxo
+            //BitIndexUtxo_class[] utxos = BitIndex_class.getUtxosByAnAddress(uri, network, destAddress);
+
+            ////AES
+            //byte[] ciphers = AES_class.aesEncryptStringToBytes(opReturnData, privateKey);
+            //string plainStr = AES_class.aesDecryptStringFromBytes(ciphers, privateKey);
 
             Console.WriteLine("press any key to exist");
             Console.ReadKey();
